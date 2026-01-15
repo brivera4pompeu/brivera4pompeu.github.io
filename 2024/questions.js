3fcf6725bf7131b3f53e81b03d17b05221c3df4e
brivera4pompeu <brivera4@iespompeufabra.cat>
2026-01-15 18:59:49 +0100

    Update questions.js

--- 2024/questions.js
+++ 2024/questions.js
@@ -1,122 +1,88 @@
 const questions = [
-    {
-        type: "questions", // Tipus de pregunta
-        category: "electrics",
-        text: `Amb un voltímetre digital es mesura cinc vegades la caiguda de tensió entre els borns
-d’una pila, i els valors obtinguts són 9.015 V, 9.025 V, 9,000 V, 8.975 V i 8.985 V. Es pot
-prendre com a resultat de la mesura, amb l’interval d’incertesa corresponent, 9.000 V? 
-`,
-        options: [
-            { text: "Sí, perquè és el tercer valor dels cinc obtinguts.", value: "a" },
-            { text: "Sí, perquè és la mitjana dels valors obtinguts.", value: "b" },
-            { text: "Sí, perquè és el valor més proper a l’última mesura.", value: "c" },
-            { text: "Sí, perquè és el valor més proper a la primera mesura.", value: "d" }
-        ],
-        correctAnswer: "b",
-        steps:`
-            \\[
-            V \\, \\frac{\\text{rev}}{\\text{min}} \\cdot \\frac{1 \\, \\text{min}}{60 \\, \\text{seg}}  = 10 \\, \\frac{\\text{rev}}{\\text{seg}}
-            \\]
-            \\[	
-            ΔV  = \\frac{\\text{V_màx-V_mín}{\\text{2}} \\; \\frac{\\text{9.025-8.975}}{\\text{2}}  \\; = \\text{0.025V}
-            \\]
-            \\[
-            V = 9.000 ± 0.025 \\, V
-            \\]
-            <br><br>
-            `           
-    },
-    {
-        type: "questions", // Tipus de pregunta
-        category: "materials",
-        text: ` Una fàbrica de rajoles produeix un model de mides 200 mm x 310 mm. Si amb una caixa d’aquestes peces es pot enrajolar una superfície de 0,992 m^2, quantes rajoles conté cada caixa com a mínim?`,
-        options: [
-            { text: "14", value: "a" },
-            { text: "15", value: "b" },
-            { text: "16", value: "c" },
-            { text: "17", value: "d" }
-        ],
-        correctAnswer: "c",
-        steps: `
-            \\[
-            A = 200 \\, \\text{mm} \\times 310 \\, \\text{mm} = 62000 \\, \\text{mm^2}
-            \\]
-            \\[
-            A = 62000 \\, \\text{mm^2} \\frac{1 \\times 10^6} {10^6} \\ = 0{,}062 \\, \\text{m^2}
-            \\]
-            \\[
-            \\Delta T = 70 \\, ^\\circ \\text{C} - 10 \\, ^\\circ \\text{C} = 60 \\, ^\\circ \\text{C}
-            \\]
-            \\[
-            Q = m \\times c_e \\times \\Delta T
-            \\]
-            \\[
-            Q = 11000 \\, \\text{g} \\times 4{,}18 \\, \\frac{\\text{J}}{\\text{g} \\cdot ^\\circ \\text{C}} \\times 60 \\, ^\\circ \\text{C}
-            \\]
-            \\[
-            Q = 11000 \\times 4{,}18 \\times 60 = 2{,}7588 \\times 10^6 \\, \\text{J}
-            \\]
-            \\[
-            W = 0{,}9 \\, \\text{kWh} \\times 3600000 \\, \\text{J/kWh} = 3240000 \\, \\text{J}
-            \\]
-            \\[
-            \\eta = \\frac{Q}{W} \\times 100
-            \\]
-            \\[
-            \\eta = \\frac{2{,}7588 \\times 10^6}{3{,}24 \\times 10^6} \\times 100 = 85{,}15 \\, \\%
-            \\]
-            <br><br>
-            <img src="ruta/a/imatgeResposta.jpg" alt="Imatge relacionada amb la resposta">`,  // Imatge opcional al final de la resposta
-        images: `<img src="ruta/a/imatgePregunta.jpg" alt="Imatge relacionada amb la pregunta">`  // Imatge opcional al final de la pregunta
-    },
-    {
-        type: "questions", // Tipus de pregunta
-        category: "materials",
-        text: `Una família que anualment generava 525 kg d’envasos, en un any ha aconseguit reduir aquesta quantitat un 60 %. Quina reducció en emissions de GEH ha obtingut respecte a l’any anterior, tenint en compte que el factor d’emissió dels envasos és de 120,09 g CO2eq/kg residu?`,
-        options: [
-            { text: "25,22 kg CO2eq", value: "a" },
-            { text: "37,83 kg CO2eq", value: "b" },
-            { text: "63,05 kg CO2eq", value: "c" },
-            { text: "88,27 kg CO2eq", value: "d" }
-        ],
-        correctAnswer: "b",
-        steps: `
-            \\[
-            \\text{Quantitat inicial d’envasos} = 525 \\, \\text{kg}
-            \\]
-            \\[
-            \\text{Percentatge de reducció} = 60\\%
-            \\]
-            \\[
-            \\text{Quantitat reduïda} = 525 \\, \\text{kg} \\times 0{,}60 = 315 \\, \\text{kg}
-            \\]
-            \\[
-            \\text{Quantitat de residu restant} = 525 \\, \\text{kg} - 315 \\, \\text{kg} = 210 \\, \\text{kg}
-            \\]
-            \\[
-            \\text{Emissions inicials} = 525 \, \\text{kg} \\times 120{,}09 \, \\text{g CO2eq/kg}
-            \\]
-            \\[
-            \\text{Emissions inicials} = 63047,25 \, \\text{g CO2eq}
-            \\]
-            \\[
-            \\text{Emissions inicials} = 63{,}05 \, \\text{kg CO2eq}
-            \\]
-            \\[
-            \\text{Emissions després de la reducció} = 210 \, \\text{kg} \\times 120{,}09 \, \\text{g CO2eq/kg}
-            \\]
-            \\[
-            \\text{Emissions després de la reducció} = 25218{,}9 \, \\text{g CO2eq}
-            \\]
-            \\[
-            \\text{Emissions després de la reducció} = 25{,}22 \, \\text{kg CO2eq}
-            \\]
-            \\[
-            \\text{Reducció en emissions} = 63{,}05 \, \\text{kg CO2eq} - 25{,}22 \, \\text{kg CO2eq} = 37{,}83 \, \\text{kg CO2eq}
-            \\]
-            <br><br>
-            <img src="ruta/a/imatgeResposta.jpg" alt="Imatge relacionada amb la resposta">`,  // Imatge opcional al final de la resposta
-        images: `<img src="ruta/a/imatgePregunta.jpg" alt="Imatge relacionada amb la pregunta">`  // Imatge opcional al final de la pregunta
-    }
+   {
+    type: "questions",
+    category: "materials",
+    text: `Amb un voltímetre digital es mesura cinc vegades la caiguda de tensió entre els borns d’una pila, i els valors obtinguts són 9,015 V, 9,025 V, 9,000 V, 8,975 V i 8,985 V. Es pot prendre com a resultat de la mesura, amb l’interval d’incertesa corresponent, 9,000 V?`,
+    options: [
+        { text: "Sí, perquè és el tercer valor dels cinc obtinguts.", value: "a" },
+        { text: "Sí, perquè és la mitjana dels valors obtinguts.", value: "b" },
+        { text: "Sí, perquè és el valor més proper a l’última mesura.", value: "c" },
+        { text: "Sí, perquè és el valor més proper a la primera mesura.", value: "d" }
+    ],
+    correctAnswer: "b",
+    steps: `
+        \\[
+        V = \\frac{9{,}015 + 9{,}025 + 9{,}000 + 8{,}975 + 8{,}985}{5}
+        \\]
+        \\[
+        V = \\frac{45{,}000}{5} = 9{,}000 \\, V
+        \\]
+        \\[
+        \\Delta V = \\frac{V_{màx} - V_{mín}}{2} = \\frac{9{,}025 - 8{,}975}{2} = 0{,}025 \\, V
+        \\]
+        \\[
+        V = (9{,}000 \\pm 0{,}025) \\, V
+        \\]
+        <br><br>`
+},
+   {
+    type: "questions",
+    category: "materials",
+    text: `Una fàbrica de rajoles produeix un model de mides 200 mm x 310 mm. Si amb una caixa d’aquestes peces es pot enrajolar una superfície de 0,992 m², quantes rajoles conté cada caixa com a mínim?`,
+    options: [
+        { text: "14", value: "a" },
+        { text: "15", value: "b" },
+        { text: "16", value: "c" },
+        { text: "17", value: "d" }
+    ],
+    correctAnswer: "c",
+    steps: `
+        \\[
+        A_{rajola} = 200 \\, \\text{mm} \\times 310 \\, \\text{mm} = 62000 \\, \\text{mm²}
+        \\]
+        \\[
+        A_{rajola} = 62000 \\, \\text{mm²} \\times \\frac{1 \\text{ m²}}{10^6 \\, \\text{mm²}} = 0{,}062 \\, \\text{m²}
+        \\]
+        \\[
+        N_{rajoles} = \\frac{0{,}992 \\, \\text{m²}}{0{,}062 \\, \\text{m²/rajola}} \\approx 16
+        \\]
+        <br><br>`
+},
+  {
+    type: "questions",
+    category: "materials",
+    text: `En una cadena de fabricació hi ha tres estacions de control de qualitat en les quals es retiren les unitats defectuoses. La taxa mitjana de rebuig de cada estació és 3 %, 1% i 0,5 %, respectivament. D’un lot inicial de 1600 unitats, quantes se’n rebutgen en total?`,
+    options: [
+        { text: "24", value: "a" },
+        { text: "48", value: "b" },
+        { text: "64", value: "c" },
+        { text: "72", value: "d" }
+    ],
+    correctAnswer: "c",
+    steps: `
+        \\[
+        N_{inicial} = 1600
+        \\]
+        \\[
+        N_{rebutjades1} = 1600 \\times 0{,}03 = 48
+        \\]
+        \\[
+        N_{restants1} = 1600 - 48 = 1552
+        \\]
+        \\[
+        N_{rebutjades2} = 1552 \\times 0{,}01 = 15{,}52 \\approx 16
+        \\]
+        \\[
+        N_{restants2} = 1552 - 16 = 1536
+        \\]
+        \\[
+        N_{rebutjades3} = 1536 \\times 0{,}005 = 7{,}68 \\approx 8
+        \\]
+        \\[
+        N_{rebutjades\ total} = 48 + 16 + 8 = 72
+        \\]
+        <br><br>`
+},
+
 ];
 
